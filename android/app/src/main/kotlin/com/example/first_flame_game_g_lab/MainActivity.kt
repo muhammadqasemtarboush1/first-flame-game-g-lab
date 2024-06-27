3ab77a14ff35a47957b36301d8c924908fcf8968
muhammad <muhammadtarboush1@gmail.com>
2024-06-27 15:41:42 +0300

    deploy

---
+++ android/app/src/main/kotlin/com/example/first_flame_game_g_lab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.first_flame_game_g_lab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:first_flame_game_g_lab/src/widgets/game__app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const GameApp());
+}
